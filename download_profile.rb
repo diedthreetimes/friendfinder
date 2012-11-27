4f3da23b625944688c6428f704188ce154483272
Sky Faber <diedthreetimes@gmail.com>
2012-11-26 16:00:59 -0800

    Download initial profile. Two ways.

---
+++ download_profile.rb
@@ -0,0 +1,56 @@
+#!/usr/bin/env ruby
+require 'oauth'
+
+# Fill the keys and secrets you retrieved after registering your app
+api_key = '78imlmkvuvhi'
+api_secret = 'BbIEylD3TBcAvVeR'
+user_token = '65d96d9f-aa71-466a-bb2c-bdd589c49b5c'
+user_secret = '4f8b3bc6-a479-4510-b58f-edd0b5cd246b'
+
+# Specify LinkedIn API endpoint
+configuration = { :site => 'https://api.linkedin.com',
+  # This tells OAuth what URL to use to gain authorization
+  :authorize_path => '/uas/oauth/authenticate',
+  # THis tells OAuth what URL to use to retrieve a request token
+  :request_token_path => '/uas/oauth/requestToken',
+  # What URL to get an accessToken
+  :access_token_path => '/uas/oauth/accessToken' }
+
+# Use your API key and secret to instantiate consumer object
+consumer = OAuth::Consumer.new(api_key, api_secret, configuration)
+
+# Use your developer token and secret to instantiate access token object
+access_token = OAuth::AccessToken.new(consumer, user_token, user_secret)
+
+# Make call to LinkedIn to retrieve your own profile
+response = access_token.get("http://api.linkedin.com/v1/people/~")
+puts response
+
+
+
+
+
+
+########## This code will allow us to retrieve arbitrary profiles  ################
+# This is a hassle so it is commented out for now
+=begin
+# To retrieve someone elses profile
+# First retrieve a request_token
+request_token = consumer.get_request_token
+
+# This url will retrieve a PIN that can be used to gain access
+# After the User signs in the PIN will be provided, this can be customized to instead redirect to a known location
+puts "Request Access URL: " + request_token.authorize_url
+
+
+#Input the pin from the console
+verifier = $stdin.gets.strip
+
+# Use the verifier to gain an access token and proceed as before
+#   Our application should save these tokens until they expire
+access_token = request_token.get_access_token(:oauth_verifier => verifier)
+
+# API call to retrieve profile using access token
+response = access_token.get("http://api.linkedin.com/v1/people/~")
+puts response
+=end
