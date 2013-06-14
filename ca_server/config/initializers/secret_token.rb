ffd1071ab42bfd7874e999f9856c91862d75c137
Sky Faber <sky.faber@gmail.com>
2013-06-13 17:16:38 -0700

    Server Skeleton

---
+++ ca_server/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CaServer::Application.config.secret_token = '53e4af090132a42426f6087231fc7b3f61675c5cee9fd41386cbb0f6bad9ad008b8689de31ee226ce1e9fe9a7f514f78a7e2ad80c0818eb483b754d916478298'
