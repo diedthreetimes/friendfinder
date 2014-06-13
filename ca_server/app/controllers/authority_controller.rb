e9a903030783e5a3a58bbcd09057042bf7912ceb
Sky Faber <sky.faber@gmail.com>
2014-06-12 17:29:44 -0700

    Add signed identity. Closes T99

--- ca_server/app/controllers/authority_controller.rb
+++ ca_server/app/controllers/authority_controller.rb
@@ -1,5 +1,6 @@
 require 'hbc_psi'
 require 'ca'
+require 'open-uri'
 
 class AuthorityController < ApplicationController
   respond_to :json, :html
@@ -28,6 +29,27 @@ class AuthorityController < ApplicationController
   def download_profile
     with_log_in do |c|
       respond_with( CA.sign(c.profile.to_json, false).to_pem )
+    end
+  end
+
+  def test_identity
+    with_log_in do |c|
+      resp = {}
+
+      resp[:profile] = c.profile
+      resp[:profile][:avatar] =  {:url => c.picture_urls(:id => c.profile.id)["all"].first}
+      resp[:profile][:site_standard_profile_request] = resp[:profile][:site_standard_profile_request][:url]
+
+      hash = Digest::SHA1.new
+      open( resp[:profile][:avatar][:url] ) { |f|
+        hash << f.read
+      }
+
+      resp[:profile][:avatar][:hash] = hash.base64digest
+
+      resp[:sig] = CA.sign( resp.as_json.to_s ).to_pem
+
+      respond_with( resp )
     end
   end
 
@@ -61,6 +83,20 @@ class AuthorityController < ApplicationController
       #resp[:connections] = c.profile( id: friends.first["id"], fields: %w(positions) )
 
       resp[:psi_message] = HbcPsi.sig_message(friends.collect {|v| v[:id]})
+
+      envelope = {}
+      envelope[:profile] = c.profile
+      envelope[:profile][:avatar] =  {:url => c.picture_urls(:id => c.profile.id)["all"].first}
+      envelope[:profile][:site_standard_profile_request] = envelope[:profile][:site_standard_profile_request][:url]
+
+      hash = Digest::SHA1.new
+      open( envelope[:profile][:avatar][:url] ) { |f|
+        hash << f.read
+      }
+
+      envelope[:profile][:avatar][:hash] = hash.base64digest
+
+      resp[:signed_identity] = CA.sign( envelope.as_json.to_s ).to_pem
 
       respond_with( resp )
     end
