2460b759b41ca95b8924c201ef965f51528b6f45
root <superoak90@gmail.com>
2015-10-16 13:54:42 -0700

    add bearer psica in server

---
+++ ca_server/extras/hbc_bearer_psi_ca.rb
@@ -0,0 +1,30 @@
+require 'ca'
+require 'digest/sha1'
+class HbcBPsiCa < HbcPsi
+
+  @@secret = "\0"
+
+  # simple way to do this.
+  def self.issue_capabilities(set)
+
+    shuffle_set = set.shuffle(random: SecureRandom.hex(23).to_i(16))
+    plaintext_set = shuffle_set.collect do |x|
+      hash_str(x, @@secret)
+    end
+
+    string_set = plaintext_set.collect do |x|
+      encode(x)
+    end.join(' ')
+
+    plaintext = plaintext_set.collect do |x|
+      x.to_s(16)
+    end.join(' ');
+
+    sig = CA.sign(string_set) # probably dont need it but for consistency.
+
+    # TODO: add timestamp?
+
+    {signed_message: sig.to_pem, plaintext: plaintext }
+  end
+
+end
