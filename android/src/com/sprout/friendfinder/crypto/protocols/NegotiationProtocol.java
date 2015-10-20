a12f14b3705c797c36a037aed2df2cb67b267eaa
Norrathep Rattanavipanon <superoak90@gmail.com>
2015-10-20 13:28:10 -0700

    use protocol callback in negotiation protocol

--- android/src/com/sprout/friendfinder/crypto/protocols/NegotiationProtocol.java
+++ android/src/com/sprout/friendfinder/crypto/protocols/NegotiationProtocol.java
@@ -5,6 +5,8 @@ import android.util.Log;
 import com.sprout.finderlib.communication.CommunicationService;
 import com.sprout.finderlib.crypto.PrivateProtocol;
 import com.sprout.friendfinder.backend.DiscoveryService.ProfileDownloadCallback;
+import com.sprout.friendfinder.backend.DiscoveryService.ProtocolCallback;
+import com.sprout.friendfinder.crypto.protocols.ProtocolManager.ProtocolType;
 
 /**
  * run this protocol before starting real protocols
@@ -12,13 +14,13 @@ import com.sprout.friendfinder.backend.DiscoveryService.ProfileDownloadCallback;
  * @author norrathep
  *
  */
-public class NegotiationProtocol extends PrivateProtocol<Void, Void, String> {
+public class NegotiationProtocol extends PrivateProtocol<Void, Void, ProtocolType> {
   
   static final String TAG = NegotiationProtocol.class.getSimpleName();
-  String yourProtocol;
-  ProfileDownloadCallback callback;
+  ProtocolType yourProtocol;
+  ProtocolCallback callback;
   
-  public NegotiationProtocol(CommunicationService s, String yourProtocol, ProfileDownloadCallback callback) {
+  public NegotiationProtocol(CommunicationService s, ProtocolType yourProtocol, ProtocolCallback callback) {
     this(TAG, s, false);
     this.yourProtocol = yourProtocol;
     this.callback = callback;
@@ -30,7 +32,7 @@ public class NegotiationProtocol extends PrivateProtocol<Void, Void, String> {
   }
 
   @Override
-  protected String conductClientTest(CommunicationService s, Void... input) {
+  protected ProtocolType conductClientTest(CommunicationService s, Void... input) {
     return conductServerTest(s, input);
   }
 
@@ -38,22 +40,21 @@ public class NegotiationProtocol extends PrivateProtocol<Void, Void, String> {
    * TODO: implement idx protocol
    */
   @Override
-  protected String conductServerTest(CommunicationService s, Void... input) {
-    s.write(yourProtocol);
-    String peerProtocol = s.readString();
-    return peerProtocol;
+  protected ProtocolType conductServerTest(CommunicationService s, Void... input) {
+    s.write(yourProtocol.name());
+    return ProtocolType.valueOf(s.readString());
   }
 
   @Override
   protected void loadSharedKeys() {}
 
   @Override
-  public String doInBackground(Void... params) {
+  public ProtocolType doInBackground(Void... params) {
     try {
       return super.doInBackground(params);
     } catch (Exception e) {
       Log.e(TAG, "Negotiation failed, ", e);
-      return null;
+      return ProtocolType.NONE;
     }
   }
 
@@ -63,13 +64,17 @@ public class NegotiationProtocol extends PrivateProtocol<Void, Void, String> {
   }
 
   @Override
-  public void onPostExecute(String result) {
+  public void onPostExecute(ProtocolType result) {
     // TODO: decide what protocol to run, right now assume both protocols have to match
-    if(result == null || !result.equals(yourProtocol)) {
-      Log.i(TAG, "Conflict: I want to run "+yourProtocol+" he wants to run "+result+", aborting");
-      callback.onError();
+    Log.i(TAG, "Result protocol is "+result.name());
+    if(result == ProtocolType.NONE) {
+      Log.i(TAG, "Error");
+      callback.onError(result);
     }
-    // maybe need to put protocol in oncomplete
-    else callback.onComplete();
+    else {
+      // return higher-priority protocol
+      ProtocolType resultProtocol = ProtocolType.max(yourProtocol, result);
+      callback.onComplete(resultProtocol);
+    }
   }
 }
